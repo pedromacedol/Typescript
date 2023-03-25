637f9a0ca7a69b33d983427594319d212dbdc799
pedromacedol <pedromacedoldev@gmail.com>
2023-03-25 11:37:08 -0300

    type variables

--- Learning_TypeScript/model.ts
+++ Learning_TypeScript/model.ts
@@ -1,10 +1,18 @@
-let displayName = "Peter";
-let inventoryType = "furniture";
-let trackingNumber = "FD123455";
-let createDate = new Date();
-let originalCost = 425;
+let displayName: string = "Peter";
+let inventoryType: string = "furniture";
+let trackingNumber: string = "FD123455";
+let createDate: Date = new Date();
+let originalCost: number = 425;
 
-function getInventoryItem(trackingNumber) {}
+function getInventoryItem(trackingNumber: string): {
+  displayName: string;
+  inventoryType: string;
+  trackingNumber: string;
+  createDate: Date;
+  originalCost: number;
+} {
+  return null;
+}
 
 function saveInventoryItem(item) {}
 
